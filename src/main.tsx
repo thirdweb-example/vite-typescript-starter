f4e752e357aefc6ec8623c97a531c6e8e7128ac7
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-03-18 01:46:38 +0100

    Create vite-typescript-starter

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
