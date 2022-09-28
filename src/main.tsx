e1a64d837c9c88610f3a9aa2f1b2f5cad63a0d94
Jarrod Watts <jarrodwatts16@gmail.com>
2022-09-28 15:19:13 +1000

    Add styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mainnet;
