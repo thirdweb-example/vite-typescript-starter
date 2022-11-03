e3e1b5c98823991bbe1ed7cc0451be05e3f4cbb6
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-11-04 00:05:37 +0100

    Update to vite v3

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-const defaultConfig = {
-  base: "./",
-  plugins: [react()],
-};
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === "serve") {
-    //dev config
-    return {
-      ...defaultConfig,
-      define: {
-        global: "globalThis",
-        process: {
-          env: "development",
-        },
-      },
-    };
-  }
-
-  //prod config
-  return { ...defaultConfig, define: { global: "globalThis" } };
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: "globalThis",
+    "process.env": {},
+  },
 });
