af110d228409aab8e8f0969c33374828a6c91981
Manan Tank <manantankm@gmail.com>
2023-07-07 01:40:05 +0530

    wcv2 mobile test

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,38 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import {
+	ThirdwebProvider,
+	// coinbaseWallet,
+	metamaskWallet,
+	rainbowWallet,
+	trustWallet,
+	walletConnect,
+	zerionWallet,
+} from '@thirdweb-dev/react';
+import './styles/globals.css';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = 'ethereum';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThirdwebProvider
+			activeChain={activeChain}
+			supportedWallets={[
+				metamaskWallet(),
+				// coinbaseWallet(),
+				walletConnect(),
+				zerionWallet(),
+				rainbowWallet(),
+				trustWallet(),
+			]}
+		>
+			<App />
+		</ThirdwebProvider>
+	</React.StrictMode>
 );
