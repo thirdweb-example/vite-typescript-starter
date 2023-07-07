9de1a3ce43fab2dbf396a099f139feafeea523d1
Manan Tank <manantankm@gmail.com>
2023-07-07 20:15:37 +0530

    add web3button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { ConnectWallet } from '@thirdweb-dev/react';
+import { ConnectWallet, Web3Button } from '@thirdweb-dev/react';
 import './styles/Home.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
 	return (
 		<main className='main'>
+			<ToastContainer theme='dark' />
 			<div className='container'>
 				<div className='header'>
 					<h1 className='title'>
@@ -23,6 +27,22 @@ export default function Home() {
 							}}
 						/>
 					</div>
+
+					<Web3Button
+						className='my-web3-button'
+						contractAddress='0x242674c150A59B9297c725be75732B29dEA25a2f' // goerli
+						action={async contract => {
+							await contract.erc1155.claim(0, 1);
+						}}
+						onError={e => {
+							toast('Error!');
+						}}
+						onSuccess={data => {
+							toast('Success!');
+						}}
+					>
+						ERC 1155 Claim (Goerli)
+					</Web3Button>
 				</div>
 			</div>
 		</main>
