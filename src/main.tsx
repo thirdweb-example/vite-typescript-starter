2c5caed199c5a156bee5501a3040f6ffb8431356
Manan Tank <manantankm@gmail.com>
2023-08-08 02:44:09 +0530

    Goerli only test

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import './styles/globals.css';
+import { Goerli } from '@thirdweb-dev/chains';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider
-      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
-    >
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThirdwebProvider
+			clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
+			activeChain={Goerli}
+			supportedChains={[Goerli]}
+		>
+			<App />
+		</ThirdwebProvider>
+	</React.StrictMode>
 );
