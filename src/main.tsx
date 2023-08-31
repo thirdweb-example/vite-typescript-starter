7215484e0518d9569b2357bf51ac950ca4cae8e2
Manan Tank <manantankm@gmail.com>
2023-08-31 19:29:31 +0530

    test paper native UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,70 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  coinbaseWallet,
+  metamaskWallet,
+  paperWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 import "./styles/globals.css";
-
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider
-      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
-    >
-      <App />
-    </ThirdwebProvider>
+    <AppContainer />
   </React.StrictMode>
 );
+
+function AppContainer() {
+  const [isAWSManaged, setIsAWSManaged] = React.useState(false);
+
+  return (
+    <ThirdwebProvider
+      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
+      supportedWallets={[
+        paperWallet({
+          paperClientId: "62db6ab5-3165-4aac-b7a5-b52bb39e8d69",
+          advancedOptions: {
+            recoveryShareManagement: isAWSManaged
+              ? "AWS_MANAGED"
+              : "USER_MANAGED",
+          },
+        }),
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+      ]}
+    >
+      <div
+        className="container"
+        style={{
+          marginBottom: "40px",
+        }}
+      >
+        <h2>Paper Recovery Share Management </h2>
+
+        <button
+          className={isAWSManaged ? "selected" : ""}
+          onClick={() => {
+            setIsAWSManaged(true);
+          }}
+        >
+          AWS_MANAGED
+        </button>
+
+        <button
+          className={!isAWSManaged ? "selected" : ""}
+          onClick={() => {
+            setIsAWSManaged(false);
+          }}
+        >
+          USER_MANAGED
+        </button>
+      </div>
+      <App />
+    </ThirdwebProvider>
+  );
+}
