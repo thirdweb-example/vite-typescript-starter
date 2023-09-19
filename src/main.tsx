aea723722b25f81c05f48a7677314e8f5289a11e
Manan Tank <manantankm@gmail.com>
2023-09-20 03:25:06 +0530

    test paper wallet google sign in

--- src/main.tsx
+++ src/main.tsx
@@ -33,8 +33,8 @@ root.render(
 
 const eoaWallets = [
   // localWallet(),
-  embeddedWallet(),
-  // paperWallet(),
+  // embeddedWallet(),
+  paperWallet(),
   // magicLink({
   //   oauthOptions: {
   //     providers: ["google", "facebook", "twitter", "apple"],
