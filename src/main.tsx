100033b690a6796e67cbba7a0350918bb8b65765
Manan Tank <manantankm@gmail.com>
2023-11-30 20:43:51 +0530

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  coinbaseWallet,
+  metamaskWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
 // This is the chain your dApp will work on.
@@ -13,11 +18,52 @@ const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
+    <AppWithProviders />
+  </React.StrictMode>
+);
+
+function AppWithProviders() {
+  const [useWC, setUseWC] = React.useState(false);
+  return (
     <ThirdwebProvider
       clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
+      supportedWallets={[
+        metamaskWallet({
+          connectionMethod: useWC ? "walletconnect" : "metamaskBrowser",
+        }),
+      ]}
     >
+      {/* two checkboxes  */}
+      <div
+        style={{
+          padding: 20,
+          border: "1px solid gray",
+          flexDirection: "column",
+          display: "flex",
+          gap: 20,
+        }}
+      >
+        <label>
+          <input
+            type="checkbox"
+            checked={useWC}
+            onChange={(e) => setUseWC(e.target.checked)}
+          />
+          Connect with WalletConnect on mobile
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={!useWC}
+            onChange={(e) => setUseWC(!e.target.checked)}
+          />
+          Connect with Metamask browser on mobile
+        </label>
+      </div>
+
       <App />
     </ThirdwebProvider>
-  </React.StrictMode>
-);
+  );
+}
