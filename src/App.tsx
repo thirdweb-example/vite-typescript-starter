7e0f312b8daa02eb17b675a7a9f595f7e67e2788
Manan Tank <manantankm@gmail.com>
2023-12-01 18:21:27 +0530

    add sign and mint buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, useWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
 
 export default function Home() {
+  const wallet = useWallet();
   return (
     <main className="main">
       <div
@@ -19,6 +20,45 @@ export default function Home() {
               }}
             />
           </div>
+        </div>
+
+        <br />
+        <div>
+          {wallet && (
+            <button
+              className="btn"
+              onClick={async () => {
+                await wallet.signMessage("hello world");
+                alert("signed successfully");
+              }}
+            >
+              sign message{" "}
+            </button>
+          )}
+
+          <br />
+          <br />
+          <br />
+
+          <Web3Button
+            contractAddress="0x88f54479F9DB46c2d97823D01CC316aa88B54a33" // mumbai
+            action={async (contract) => {
+              await contract.erc1155.claim(0, 1);
+              alert("claimed successfully");
+            }}
+            onError={(e) => {
+              alert("Error");
+              console.error(e);
+            }}
+            onSuccess={(data) => {
+              alert("minted successfully");
+            }}
+            onSubmit={() => {
+              console.log("Submitting");
+            }}
+          >
+            ERC1155: Claim 1 NFT
+          </Web3Button>
         </div>
       </div>
     </main>
